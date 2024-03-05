7f97e525990121f2866899c5d4281355638858db
Manin Gauba <manindgauba@gmail.com>
2024-03-05 14:15:13 -0500

    created the generate svg function

--- index.js
+++ index.js
@@ -8,26 +8,63 @@ const promptUser = () => {
     {
       type: "input",
       name: "text",
-      message: "Enter up to three characters for your logo text.",
+      message: "What is your logo",
     },
     {
       type: "input",
       name: "text_color",
-      message:
-        "Enter text color preference. (Only color keyword or hexidecimals will render properly)",
+      message: "Choose your color",
     },
     {
       type: "list",
       name: "shape",
-      message: "Select logo shape.",
+      message: "What shape would you like?",
       choices: ["Triangle", "Square", "Circle"],
     },
     {
       type: "input",
       name: "shape_color",
       message:
-        "Enter shape color preference. (Only color keyword or hexidecimals will render properly)",
+        "Enter a shape color preference.",
     },
   ]);
 };
 
+
+const init = () => {
+    promptUser()
+      .then((res) =>
+        writeFile(`logo_${res.text.toUpperCase()}.svg`, generateSVG(res))
+      )
+      .then(() => console.log(`\n"Logo has been created!"`))
+      .catch((err) => console.error(err));
+  };
+  
+  function generateSVG(res) {
+      let shape;
+      if (res.shape === "Triangle") {
+        shape = new Triangle();
+        console.log(shape);
+      } else if (res.shape === "Square") {
+        shape = new Square();
+        console.log(shape);
+      } else {
+        shape = new Circle();
+        console.log(shape);
+      }
+      shape.setColor(res.shape_color);
+    
+      return ` <svg
+        width="300"
+        height="200"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+    ><g>${shape.render()}
+    <text x="150" y="130" text-anchor="middle" font-size="40" fill="${
+        res.text_color
+      }">${res.text.toUpperCase()}</text>
+    </g></svg>
+    `;
+    }
+    
+  init();
